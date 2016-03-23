568d6329be6a8760283e12cd7a9b911df74d53a2
nicoleei <nicoleefang@gmail.com>
2016-03-24 07:20:22 +0800

    add tpl

---
+++ iframe/pages/table.js
@@ -0,0 +1,46 @@
+tpl = ''
+
+$.map(data, function(_item){
+  tpl += '<tr>' +
+            '<td>' + _item.title + '</td>' +
+            '<td>' + _item.name + '</td>' +
+          '</tr>'
+})
+
+$('#test').html('<table>'+tpl+'</table>')
+
+$.ajax({
+  type: 'post',
+  url: 'tstst.php',
+  data:{
+    id: 111
+  },
+  dataType: 'json',
+  success: function(data){
+    tpl = ''
+
+$.map(data, function(_item){
+  tpl += '<tr>' +
+            '<td>' + _item.title + '</td>' +
+            '<td>' + _item.name + '</td>' +
+          '</tr>'
+})
+
+$('#test').html('<table>'+tpl+'</table>')
+  }
+})
+
+// <table width="200" border="1" cellspacing="0" cellpadding="0">
+//   <tr>
+//     <th scope="col">&nbsp;</th>
+//     <th scope="col">&nbsp;</th>
+//   </tr>
+//   <tr>
+//     <td>&nbsp;</td>
+//     <td>&nbsp;</td>
+//   </tr>
+//   <tr>
+//     <td>&nbsp;</td>
+//     <td>&nbsp;</td>
+//   </tr>
+// </table>
